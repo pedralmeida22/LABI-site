e5a418f727d87e11daa8adb2934c5f3a5c5a0ad6
Pedro Almeida <pedro22@ua.pt>
2017-12-21 14:57:53 +0000

    Barra de navegação, cores, letra

--- proj1/HTML/projeto1.js
+++ proj1/HTML/projeto1.js
@@ -1,1 +1,19 @@
-js
+var map = new L.Map("oMeuMapa", {center: [40.633258,-8.659097],zoom: 15});
+var osmUrl="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"; 
+var osmAttrib="Map data OpenStreetMap contributors"; 
+var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+var iconeUA = L.icon({ iconUrl: "http://xcoa.av.it.pt/ua.png" });
+	
+map.addLayer(osm);
+	
+var pontos = [ 
+	L.marker([40.633258, -8.659097]), 
+];
+
+for(i in pontos) {
+	pontos[i].addTo(map); 
+}
+
+L.marker([40.633258, -8.659097], {icon: iconeUA}).bindPopup("DETI") 
+
+
