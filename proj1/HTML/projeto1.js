d8f4a195273d382ea4be3f4496ad9dc92c23b0c9
Pedro Almeida <pedro22@ua.pt>
2017-12-21 22:01:56 +0000

    Página Inicial, cores, Evolução dos Sistemas-texto, jumbotron, tabela, imagens

--- proj1/HTML/projeto1.js
+++ proj1/HTML/projeto1.js
@@ -1,3 +1,4 @@
+//mapa
 var map = new L.Map("oMeuMapa", {center: [40.633258,-8.659097],zoom: 15});
 var osmUrl="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"; 
 var osmAttrib="Map data OpenStreetMap contributors"; 
@@ -15,5 +16,6 @@ for(i in pontos) {
 }
 
 L.marker([40.633258, -8.659097], {icon: iconeUA}).bindPopup("DETI") 
+//fim mapa
 
 
